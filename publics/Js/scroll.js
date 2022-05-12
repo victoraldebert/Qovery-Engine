a982716fc41cabbcdefd62d406f14c2f7fc3d103
Victor <91267367+victoraldebert@users.noreply.github.com>
2022-05-12 11:09:30 +0200

    Changing the access path and adding animations

---
+++ publics/Js/scroll.js
@@ -0,0 +1,54 @@
+const sr = ScrollReveal();
+
+sr.reveal('.image-wrapper', {
+    origin: 'top',
+    distance: '50px',
+    scale: 0.87,
+    reset: true,
+    duration: 2500  
+});    
+
+sr.reveal('.carbon-code', {
+    origin: 'bottom',
+    distance: '50px',
+    scale: 0.87,
+    reset: true,
+    duration: 2500  
+});    
+
+sr.reveal('.scroll-1', {
+    origin: 'left',
+    distance: '50px',
+    scale: 0.87,
+    reset: true,
+    duration: 2500  
+});    
+
+sr.reveal('.scroll-2', {
+    origin: 'right',
+    distance: '50px',
+    scale: 0.87,
+    reset: true,
+    duration: 2500  
+});    
+
+sr.reveal('.scroll-3', {
+    origin: 'left',
+    distance: '50px',
+    scale: 0.87,
+    reset: true,
+    duration: 2500  
+});    
+
+sr.reveal('.scroll-4', {
+    origin: 'right',
+    distance: '50px',
+    scale: 0.87,
+    reset: true,
+    duration: 2500  
+});    
+
+
+
+
+
